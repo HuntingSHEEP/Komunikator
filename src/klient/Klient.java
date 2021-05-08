d51019ae1a40c5109c70347f2120199c2156bad6
HuntingSHEEP <a.glowacki98@gmail.com>
2021-05-08 12:09:46 +0200

    oj sporo zmian

--- src/klient/Klient.java
+++ src/klient/Klient.java
@@ -1,37 +1,59 @@
 package klient;
 
-import java.io.*;
-import java.net.*;
+import java.net.ConnectException;
+import java.net.Socket;
 
-public class Klient
-{
-   public static final int PORT=50007;
-   public static final String HOST = "127.0.0.1";
+public class Klient {
 
-   public static void main(String[] args) throws IOException
-   {
-      //TODO: Klient łączy pod wskazany adres  na porcie Z PULI PORTÓW; POWIEDZMY NA POCZĄTEK STAŁA PULA PORTÓW;  PRÓBUJEMY ŁĄCZYĆ SIĘ Po kolei do skutku
-       //TODO: zamykanie połączenie od strony klienta za pomocą jednej komendy
+    public static final String HOST = "127.0.0.1";
+    public static final int[] PORT={50007, 50008, 50009, 50010};
+    int[] flagiPortow = {0, 0, 0, 0}; //0: nie sprawdzano, 1:zajęty
+    int liczbaPortow = 4;
 
-      //nawiazanie polaczenia z serwerem
-      Socket sock;
-      sock=new Socket(HOST,PORT);
-      System.out.println("Nawiazalem polaczenie: "+sock);
+    Klient(){
+        polaczSieZSerwerem();
+    }
+
+    private void polaczSieZSerwerem() {
+        int i = 0;
+        boolean flagaPetli = true;
+        while(flagaPetli){
+            if(flagiPortow[i] == 0){
+                //TODO: SPRAWDZANIE CZY JEDNAK NIE JEST ZAJĘTY PORT
+
+                try{
+                    //nawiazanie polaczenia z serwerem
+                    Socket sock;
+                    sock=new Socket(HOST,PORT[i]);
+                    System.out.println("Nawiazalem polaczenie: "+sock);
+
+                    //tworzenie wątku nadającego
+                    NadawajK nadawanie = new NadawajK(sock);
+                    nadawanie.start();
+
+                    //tworzenie watka odbierajacego
+                    OdbiorK odbieranie = new OdbiorK(sock);
+                    odbieranie.start();
+
+                    //przekazanie referencji do watku
+                    nadawanie.podajWatekOdbierajacy(odbieranie);
+                    odbieranie.setWatekNadajacy(nadawanie);
+
+                    flagiPortow[i] = 1;
+                    flagaPetli = false;
+                }catch (ConnectException con){
+                    flagiPortow[i] = 1;
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }else{
+                i++;
+                i %= liczbaPortow;
+            }
+
+        }
+
+    }
 
 
-      //tworzenie wątku nadającego
-      NadawajK nadawanie = new NadawajK(sock);
-      nadawanie.start();
-
-      //tworzenie watka odbierajacego
-      OdbiorK odbieranie = new OdbiorK(sock);
-      odbieranie.start();
-
-      //przekazanie referencji do watku
-       nadawanie.podajWatekOdbierajacy(odbieranie);
-       odbieranie.setWatekNadajacy(nadawanie);
-
-      //zamykanie polaczenia
-      //sock.close();
-   }
 }
--- src/serwer/Serwer.java
+++ src/serwer/Serwer.java
@@ -1,58 +1,68 @@
 package serwer;
 
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Serwer  {
+    public static final int[] PORT={50007, 50008, 50009, 50010};
+    int[] flagiPortow = {0, 0, 0, 0}; //0: nie sprawdzano, 1:zajęty
+    int liczbaPortow = 4;
 
 
-public class Serwer
-{
-   public static final int PORT=50007;
-
-   Nadawaj watekNadajacy;
-   Odbior watekOdbierajacy;
-
-   public static void main(String args[]) throws IOException
-   {
-      //tworzenie gniazda serwerowego
-      ServerSocket serv;
-      serv=new ServerSocket(PORT);
-
-      //oczekiwanie na polaczenie i tworzenie gniazda sieciowego
-      System.out.println("Nasluchuje: "+serv);
-      Socket sock;
-
-      sock=serv.accept();
-      System.out.println("Jest polaczenie: "+sock);
-
-      //tworzenie wątku nadającego
-      //nadaj(sock);
-
-      Nadawaj watekNadajacy = new Nadawaj(sock);
-      watekNadajacy.start();
-
-      //tworzenie watka odbierajacego
-      //odbior(sock);
-
-      Odbior watekOdbierajacy = new Odbior(sock);
-      watekOdbierajacy.setWatekNadajacy(watekNadajacy);
-      watekNadajacy.podajWatekOdbierajacy(watekOdbierajacy);
-      watekOdbierajacy.start();
+    Nadawaj[] watkiNadajace = new Nadawaj[4];
+    Odbior[] watkiOdbierajace = new Odbior[4];
 
 
-      //zamykanie polaczenia
-      //serv.close();
-      //sock.close();
-   }
 
-   public static synchronized void odbior(Socket sock){
-     try{
-        new Odbior(sock).start();
-     }catch(Exception e){System.out.println("sync serwer.Odbior");}
-   }
+    Serwer()  {
+        obslugaPolaczen();
+    }
 
-   public static synchronized void nadaj(Socket sock){
-     try{
-        new Nadawaj(sock).start();
-     }catch(Exception e){System.out.println("sync Nadaj");}
-   }
+    private void obslugaPolaczen() {
+        int i = 0;
+
+        while(true){
+            if(flagiPortow[i] == 0){
+                try{
+                    //tworzenie gniazda serwerowego
+                    ServerSocket serv;
+                    serv=new ServerSocket(PORT[i]);
+
+                    //oczekiwanie na polaczenie i tworzenie gniazda sieciowego
+                    System.out.println("Nasluchuje: "+serv);
+                    Socket sock;
+
+                    sock=serv.accept();
+                    System.out.println("Jest polaczenie: "+sock);
+
+                    //tworzenie wątku nadającego
+                    //nadaj(sock);
+
+                    Nadawaj watekNadajacy = new Nadawaj(sock);
+                    watekNadajacy.start();
+
+                    //tworzenie watka odbierajacego
+                    //odbior(sock);
+
+                    Odbior watekOdbierajacy = new Odbior(sock);
+                    watekOdbierajacy.setWatekNadajacy(watekNadajacy);
+                    watekNadajacy.podajWatekOdbierajacy(watekOdbierajacy);
+                    watekOdbierajacy.start();
+
+                    watkiNadajace[i] = watekNadajacy;
+                    watkiOdbierajace[i] = watekOdbierajacy;
+
+                    flagiPortow[i] = 1;
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }else{
+                i++;
+                i %= liczbaPortow;
+            }
+        }
+
+    }
 }
