6bc0d6ef13f3dcba72777f6bf9fd5e299db89f43
HuntingSHEEP <a.glowacki98@gmail.com>
2021-05-08 15:47:51 +0200

    nie działa zabijanie klienta od strony serwera

--- src/klient/Klient.java
+++ src/klient/Klient.java
@@ -13,9 +13,8 @@ public class Klient {
 
      */
     public static final String HOST = "127.0.0.1";
-    public static final int[] PORT={50007};
-    int[] flagiPortow = {0}; //0: nie sprawdzano, 1:zajęty
-    int liczbaPortow = 1;
+    public static final int PORT=50007;
+
 
     Klient(){
         polaczSieZSerwerem();
@@ -24,44 +23,29 @@ public class Klient {
     private void polaczSieZSerwerem() {
         int i = 0;
         boolean flagaPetli = true;
+
         while(flagaPetli){
-            if(flagiPortow[i] == 0){
-                //TODO: SPRAWDZANIE CZY JEDNAK NIE JEST ZAJĘTY PORT
+            try{
+                //nawiazanie polaczenia z serwerem
+                Socket sock;
+                sock=new Socket(HOST,PORT);
+                System.out.println("Nawiazalem polaczenie: "+sock);
 
-                try{
-                    //nawiazanie polaczenia z serwerem
-                    Socket sock;
-                    sock=new Socket(HOST,PORT[i]);
-                    System.out.println("Nawiazalem polaczenie: "+sock);
+                //tworzenie wątku nadającego
+                NadawajK nadawanie = new NadawajK(sock);
+                nadawanie.start();
 
-                    //tworzenie wątku nadającego
-                    NadawajK nadawanie = new NadawajK(sock);
-                    nadawanie.start();
+                //tworzenie watka odbierajacego
+                OdbiorK odbieranie = new OdbiorK(sock);
+                odbieranie.start();
 
-                    //tworzenie watka odbierajacego
-                    OdbiorK odbieranie = new OdbiorK(sock);
-                    odbieranie.start();
+                //przekazanie referencji do watku
+                nadawanie.podajWatekOdbierajacy(odbieranie);
+                odbieranie.setWatekNadajacy(nadawanie);
 
-                    //przekazanie referencji do watku
-                    nadawanie.podajWatekOdbierajacy(odbieranie);
-                    odbieranie.setWatekNadajacy(nadawanie);
-
-                    flagiPortow[i] = 1;
-                    flagaPetli = false;
-                }catch (ConnectException con){
-                    flagiPortow[i] = 1;
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }else{
-                i++;
-                //i %= liczbaPortow;
-            }
-            if (i >= liczbaPortow ){
-                for (int j = 0;j<liczbaPortow;j++){
-                    flagiPortow[j] = 0;
-                }
-                i=0;
+                flagaPetli = false;
+            }catch (Exception e){
+                e.printStackTrace();
             }
 
         }
