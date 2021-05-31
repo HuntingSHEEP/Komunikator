da190e868b6713da37165dc1d5452631495bba40
KroKrzy <kkrok.cisco2017@gmail.com>
2021-05-31 11:51:27 +0200

    Singleton

---
+++ ANDROID/app/src/main/java/com/example/sheeptalk/Rozmowy.java
@@ -0,0 +1,18 @@
+package com.example.sheeptalk.logic.klient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.sheeptalk.logic.klient.AsyncClient;
+import com.example.sheeptalk.logic.klient.Klient;
+import com.example.sheeptalk.logic.klient.Nadawaj;
+import com.example.sheeptalk.logic.klient.Odbior;
+
+public class Rozmowy extends AppCompatActivity{
+
+}
---
+++ ANDROID/app/src/main/java/com/example/sheeptalk/logic/klient/Singleton.java
@@ -0,0 +1,2 @@
+package com.example.sheeptalk.logic.klient;public class Singleton {
+}
